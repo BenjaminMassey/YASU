61925edbc9233809bdcdafa6070e96f414ff379a
Benjamin Massey <benjamin.w.massey@gmail.com>
2024-10-09 23:13:07 -0700

    Used Clap to have command line arguments for replay settings.

--- src/replay.rs
+++ src/replay.rs
@@ -1,17 +1,26 @@
-const IN_VIDEOS_PATH: &str = "C:\\Users\\User\\Videos\\";
-const VIDEO_EXT: &str = ".mp4";
-const PAUSE_TIME: u64 = 4;
+use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+pub struct Args {
+    #[clap(long, default_value_t = String::from("C:\\Users\\User\\Videos\\"))]
+    pub obs_replay_path: String,
+    #[clap(long, default_value_t = String::from(".mp4"))]
+    pub obs_video_ext: String,
+    #[clap(long, default_value_t = 4u64)]
+    pub replay_save_delay: u64,
+}
 
 pub fn perform() {
+    let args = Args::parse();
     println!("Pausing for replay saving...");
-    std::thread::sleep(std::time::Duration::from_secs(PAUSE_TIME));
+    std::thread::sleep(std::time::Duration::from_secs(args.replay_save_delay));
     println!("Done pausing.");
-    let recent = recent_path();
+    let recent = recent_path(&args);
     if recent.is_none() {
         println!("Failed to save replay because no recent video replay file found.");
         return;
     }
-    let target = crate::OUT_DIR.to_owned() + "replay" + VIDEO_EXT;
+    let target = crate::OUT_DIR.to_owned() + "replay" + &args.obs_video_ext;
     let result = std::fs::copy(&recent.unwrap(), &target);
     if let Err(e) = result {
         println!("Error in replay copying: {e:?}");
@@ -20,8 +29,8 @@ pub fn perform() {
     }
 }
 
-fn recent_path() -> Option<String> {
-    let param = IN_VIDEOS_PATH.to_owned() + "Replay*" + VIDEO_EXT;
+fn recent_path(args: &Args) -> Option<String> {
+    let param = args.obs_replay_path.to_owned() + "Replay*" + &args.obs_video_ext;
     let vid_paths = glob::glob(&param)
         .unwrap()
         .filter_map(std::result::Result::ok);
